6a81d3f49d6f7dd3784d964bca4cea87dd5001f2
Chris Vallianatos <cvallianatos@gmail.com>
2023-05-31 07:41:35 -0400

    Add files via upload

---
+++ start-server.sh
@@ -0,0 +1,3 @@
+export PATH= "PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/Library/Frameworks/Python.framework/Versions/3.11/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/GPAC.app/Contents/MacOS/:/usr/local/go/bin:/Library/Apple/usr/bin:/Users/chris/.cargo/bin"
+
+python server.py
